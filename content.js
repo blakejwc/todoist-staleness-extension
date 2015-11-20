73313892bae559d9a6f9e37d1d8629b0ff700b39
Blake Chambers <blake.chambers@vecna.com>
2015-11-20 16:51:07 -0500

    Used black sun for 0 days, comet for >9 days, coffee for recurring tasks, and added gradient color to square grid

--- content.js
+++ content.js
@@ -6,32 +6,30 @@ var observer = new MutationObserver(function() {
       taskCompletedList = taskEditor.find('div#completed_app');
 
   function drawSquare(ctx, r, c) {
-    ctx.beginPath();
-    ctx.rect(c*7, r*7, 4, 4);
-    ctx.fill();
-  }
+    /**
+     * |* * *|
+     * |* x x| ==> blockIndex = 5
+     * |x x x|
+     *         Although this calculation is based on how drawSquares works
+     */
+    var blockIndex = ((3-r)*3+(3-c)),
+        rgbValue = 255 - blockIndex*18;
 
-  function fillCanvas(ctx) {
-    ctx.beginPath();
-    ctx.rect(4, 4, 10, 10);
-    ctx.fill();
+    // color should be increasing gradient (white -> black) as the blockIndex increase
+    ctx.fillStyle = 'rgb(' + rgbValue + ','+ rgbValue + ','+ rgbValue +')';
+    ctx.fillRect(c*7, r*7, 4, 4);
   }
 
   function drawSquares(canvas, count) {
-    var r,
+    var r, c,
         numSq = count,
         ctx = canvas.getContext('2d');
-        ctx.fillStyle = '#000000';
-    if (count > 9) {
-      fillCanvas(ctx);
-    } else {
-      for (r = 0; numSq > 0 && r < 3; r++) {
-          var c;
-          for (c = 0; numSq > 0 && c < 3; c++) {
-              drawSquare(ctx, r, c);
-              numSq--;
-          }
-      }
+
+    for (r = 2; numSq > 0 && r >= 0; r--) {
+        for (c = 2; numSq > 0 && c >= 0; c--) {
+            drawSquare(ctx, r, c);
+            numSq--;
+        }
     }
   }
 
@@ -47,7 +45,7 @@ var observer = new MutationObserver(function() {
     // The reorder item is not an actual task
     taskEditor.find('.task_item').not('.reorder_item').each(function () {
       var taskTableRow = $(this).find('tr'),
-          itemId, item, dateAdded, canvas, stalenessEl;
+          itemId, item, dateAdded, canvas, stalenessEl, stalenessContainer, daysBetween;
 
       // skip if we already added a staleness container
       if (taskTableRow.find('.staleness-container').length === 0) {
@@ -59,25 +57,45 @@ var observer = new MutationObserver(function() {
           itemId = $(this)[0].id.slice(5);
           item = itemCache[itemId] || {};
           dateAdded = new Date(item.date_added);
+          daysBetween = getDaysBetween(today, dateAdded);
 
-          // Add an element to indicate staleness
-          taskTableRow.prepend('<td class="staleness-container"><canvas id="staleness"></canvas></td>');
+          if (daysBetween === 0) {
+            // 0 days get black sun
+            stalenessContainer = taskTableRow.prepend('<td class="staleness-container">&#9728</td>').find('.staleness-container');
+            stalenessContainer.css('padding-right', '1px');
+            stalenessContainer.css('padding-top', '4px');
+            stalenessContainer.css({'font-size': 24});
+          }
+          else if (daysBetween > 9) {
+            // Greate that 9 days get comet of dooooom!
+            stalenessContainer = taskTableRow.prepend('<td class="staleness-container">&#9732</td>').find('.staleness-container');
+            stalenessContainer.css('padding-right', '1px');
+            stalenessContainer.css('padding-top', '4px');
+            stalenessContainer.css({'font-size': 24});
+          } else {
+            // Add an element to indicate staleness
+            taskTableRow.prepend('<td class="staleness-container"><canvas id="staleness"></canvas></td>');
 
-          // edit the staleness containter
-          stalenessEl = taskTableRow.find('td.staleness-container');
-          stalenessEl.css('padding-right', '5px');
-          stalenessEl.css('padding-top', '15px');
+            // edit the staleness containter
+            stalenessEl = taskTableRow.find('td.staleness-container');
+            stalenessEl.css('padding-right', '5px');
+            stalenessEl.css('padding-top', '15px');
 
-          // edit the staleness canvas
-          canvas = stalenessEl.find('#staleness')[0];
-          canvas.width = 18;
-          canvas.height = 18;
+            // edit the staleness canvas
+            canvas = stalenessEl.find('#staleness')[0];
+            canvas.width = 18;
+            canvas.height = 18;
 
-          // add the staleness indicator
-          drawSquares(canvas, getDaysBetween(today, dateAdded));
+            // add the staleness indicator
+
+            drawSquares(canvas, daysBetween);
+          }
         } else {
-          // Handle the recurreing tasks by creating invisible space
-          taskTableRow.prepend('<td class="staleness-container"></td>').find('.staleness-container').css('padding-right', '23px');
+          // Recurring tasks get coffee character
+          stalenessContainer = taskTableRow.prepend('<td class="staleness-container">&#9749</td>').find('.staleness-container');
+          stalenessContainer.css('padding-right', '1px');
+          stalenessContainer.css('padding-top', '4px');
+          stalenessContainer.css({'font-size': 24});
         }
       }
     });
